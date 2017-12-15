e8e688cab9031096a6b1da40466baee2edc3d0f3
ldpeng <20556046@qq.com>
2017-12-15 11:12:06 +0800

    修改评价插件

--- JavaScript/五角星评分案例/jquery.util.evaluation.js
+++ JavaScript/五角星评分案例/jquery.util.evaluation.js
@@ -3,21 +3,25 @@
     $.evaluation = function (el, options) {
         var settings = $.extend({
             count: 5,   //评价级数
-            size: 30,   //字体大小
-            color: "yellow", //字体颜色
+            size: 20,   //字体大小
+            color: "#ffc002", //字体颜色
             emptyText: "☆",  //空白时文本，可以是一个图片路径，或者一个数组，存储每一个级别的内容
-            fillText: "★"   //填充时的文本，可以是一个图片路径，或者一个数组，存储每一个级别的内容
+            fillText: "★",   //填充时的文本，可以是一个图片路径，或者一个数组，存储每一个级别的内容
+            defaultVal: 0,  //默认级数
+            clickable: true //是否可点击进行评价
         }, options || {});
 
-        el = $("#" + el);
+        el = isString(el) ? $("#" + el) : $(el);
 
         //构建评价html
         var ul = $("<ul>").css({
             listStyle: "none",
             fontSize: settings.size + "px",
-            color: settings.color,
-            cursor: "pointer"
+            color: settings.color
         });
+
+        if (settings.clickable) ul.css("cursor", "pointer");
+
         for (var i = 0; i < settings.count; i++) {
             var html = evaluationHtml(settings.emptyText, i);
             if (html) {
@@ -26,33 +30,46 @@
         }
         el.append(ul);
 
-        ul.find("li").mouseenter(function () {
-            //鼠标移入: 自己和前面的兄弟变实心，其余变空心
-            $(this).html(evaluationHtml(settings.fillText, $(this).attr("evaluationVal") - 1));
-            $(this).prevAll().each(function () {
-                $(this).html(evaluationHtml(settings.fillText, $(this).attr("evaluationVal") - 1));
-            });
-            $(this).nextAll().each(function () {
-                $(this).html(evaluationHtml(settings.emptyText, $(this).attr("evaluationVal") - 1));
-            });
-        }).click(function () {
-            //鼠标点击后，把自己添加clicked类，其余的清除clicked类
-            $(this).addClass('clicked').siblings().removeClass('clicked');
-        });
-
-        //当鼠标移开评分控件时，实心显示到被点击的五角星的上
-        ul.mouseleave(function () {
-            var evaluationVal = el.evaluationVal();
+        el.setEvaluationVal = function (val) {
             ul.find("li").each(function () {
                 var v = $(this).attr("evaluationVal");
-                if (v <= evaluationVal) $(this).html(evaluationHtml(settings.fillText, v - 1));
-                else $(this).html(evaluationHtml(settings.emptyText, v - 1));
+                if (v < val) {
+                    $(this).html(evaluationHtml(settings.fillText, v - 1));
+                } else if (v == val) {
+                    $(this).html(evaluationHtml(settings.fillText, v - 1));
+                    $(this).addClass('clicked').siblings().removeClass('clicked');
+                } else {
+                    $(this).html(evaluationHtml(settings.emptyText, v - 1));
+                }
             });
-        });
+        };
 
         el.evaluationVal = function () {
             return ul.find(".clicked").attr("evaluationVal") || 0;
+        };
+
+        if (settings.clickable) {
+            ul.find("li").mouseenter(function () {
+                //鼠标移入: 自己和前面的兄弟变实心，其余变空心
+                $(this).html(evaluationHtml(settings.fillText, $(this).attr("evaluationVal") - 1));
+                $(this).prevAll().each(function () {
+                    $(this).html(evaluationHtml(settings.fillText, $(this).attr("evaluationVal") - 1));
+                });
+                $(this).nextAll().each(function () {
+                    $(this).html(evaluationHtml(settings.emptyText, $(this).attr("evaluationVal") - 1));
+                });
+            }).click(function () {
+                //鼠标点击后，把自己添加clicked类，其余的清除clicked类
+                $(this).addClass('clicked').siblings().removeClass('clicked');
+            });
+
+            //当鼠标移开评分控件时，实心显示到被点击的五角星的上
+            ul.mouseleave(function () {
+                el.setEvaluationVal(el.evaluationVal());
+            });
         }
+
+        if(settings.defaultVal && settings.defaultVal > 0) el.setEvaluationVal(settings.defaultVal);
 
         return el;
     };
